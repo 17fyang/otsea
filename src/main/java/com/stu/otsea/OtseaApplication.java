c182d85172459357226fa528cbe161b648657bcf
17fyang <1099640098@qq.com>
2020-10-25 19:18:03 +0800

    item init

---
+++ src/main/java/com/stu/otsea/OtseaApplication.java
@@ -0,0 +1,13 @@
+package com.stu.otsea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtseaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtseaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stu/otsea/OtseaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stu.otsea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtseaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
